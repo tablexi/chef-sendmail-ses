1ad89f7ebbff2b07d302288af999e297e7cee22d
Lloyd Philbrook <lloyd@tablexi.com>
2015-10-14 10:40:15 -0500

    fix license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'sendmail-ses'
 maintainer 'TABLE XI'
 maintainer_email 'sysadmin@tablexi.com'
-license 'All rights reserved'
+license 'GNU Public License 3.0'
 description 'Installs/Configures sendmail-ses'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,9 +2,20 @@
 # Cookbook Name:: sendmail-ses
 # Recipe:: default
 #
-# Copyright (C) 2013 TABLE XI
+# Copyright 2015, TableXI
 #
-# All rights reserved - Do Not Redistribute
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 if node.attribute? 'sendmail_ses'
