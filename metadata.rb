96a694343d68f1febc1620cf19d55153547ab18e
Lloyd Philbrook <lloyd@tablexi.com>
2016-05-12 10:03:59 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sysadmin@tablexi.com'
 license 'GNU Public License 3.0'
 description 'Installs/Configures sendmail-ses'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.2.2'
 
 supports 'amazon'
