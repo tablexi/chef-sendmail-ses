1af06bbad8489f0173c85013a86d6c3a331ce1a9
Lloyd Philbrook <lloyd@tablexi.com>
2017-02-17 12:24:06 -0600

    Fix foodcritic errors

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license 'GNU Public License 3.0'
 description 'Installs/Configures sendmail-ses'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.2'
+issues_url 'https://github.com/tablexi/chef-sendmail-ses/issues'
+source_url 'https://github.com/tablexi/chef-sendmail-ses/'
 
 supports 'amazon'
