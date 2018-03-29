4227f080e923ea13556603e1ea57f677c5d114f8
Lloyd Philbrook <lloyd@tablexi.com>
2018-03-29 16:14:34 -0700

    UPDATE foodcritic

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,6 @@ version '0.2.3'
 issues_url 'https://github.com/tablexi/chef-sendmail-ses/issues'
 source_url 'https://github.com/tablexi/chef-sendmail-ses/'
 
+chef_version '>= 12'
+
 supports 'amazon'
